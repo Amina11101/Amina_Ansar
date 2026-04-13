7b2d26fedc8498459ae868b5d361bb7044a8b61a
Amina <amina@example.com>
2026-04-13 19:21:10 +0200

    Fix: conditional base path for dev and production

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Amina_Ansar/',
+  base: process.env.NODE_ENV === 'production' ? '/Amina_Ansar/' : '/',
   plugins: [react()],
   build: {
     cssMinify: false,
